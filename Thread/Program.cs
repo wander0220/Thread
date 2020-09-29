2c83d7c423914f4a9e2097a21379f78795580763
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-09-29 10:15:24 +0900

    초기 커밋.

---
+++ Thread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread ta = new Thread(TestMethod);
+        }
+
+        public void TestMethod()
+        {
+
+        }
+    }
+}
